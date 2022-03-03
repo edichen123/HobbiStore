8e6323c6a919391f114a4e1b4c997ef481979933
edichen123 <edichenjl@outlook.com>
2022-03-03 09:46:29 +0800

    added id for fake data

--- client/src/Components/SingleProduct.jsx
+++ client/src/Components/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const SingleProduct = (props) => {
-  // console.log(props.item)
+  console.log(props.item)
   return (
     <div className=' flex-1 m-1 min-w-[280px] h-[350px] flex items-center justify-center bg-slate-100 relative'>
       SingleProduct
--- client/src/Components/fakeproducts.js
+++ client/src/Components/fakeproducts.js
@@ -1,5 +1,6 @@
 const products = [
   {
+    id: 1,
     name: "Aer Day Pack 2 X-Pac",
     description: "a backpack",
     price: 250,
@@ -8,6 +9,7 @@ const products = [
       "https://images.squarespace-cdn.com/content/v1/5362a66ee4b05f0813543305/1627827826736-PBOS3X82MIA4V6PRY4ML/AER91008_daypack2xpac_34hero.jpg?format=1000w",
   },
   {
+    id: 2,
     name: "Aer Daypack 2",
     description: "a backpack",
     price: 220,
@@ -16,6 +18,7 @@ const products = [
       "https://images.squarespace-cdn.com/content/v1/5362a66ee4b05f0813543305/1583523409468-4PMNR9DDYD88FAXBQJBB/AER31009_daypack2_black_front+.jpg?format=1000w",
   },
   {
+    id: 3,
     name: "Evergoods Civic Panel Loader 24L V2",
     description: "a backpack",
     price: 400,
@@ -24,6 +27,7 @@ const products = [
       "https://cdn.shopify.com/s/files/1/1567/6353/products/silhsidview_1200x.jpg?v=1592415562",
   },
   {
+    id: 4,
     name: "Evergoods Civic Access Pouch 2L",
     description: "a Pouch",
     price: 90,
@@ -32,6 +36,7 @@ const products = [
       "https://cdn.shopify.com/s/files/1/1567/6353/products/Cap2whitewebsite_1200x.jpg?v=1603810103",
   },
   {
+    id: 5,
     name: "Able Carry Thirteen Daybag",
     description: "a backpack",
     price: 150,
@@ -40,6 +45,7 @@ const products = [
       "https://cdn.shopify.com/s/files/1/0028/1971/9281/products/302101102_-_13L_XPAC_Deep_Black_-_Front_Pocket_1200x.png?v=1624593774",
   },
   {
+    id: 6,
     name: "Able Carry Max Backpack",
     description: "a backpack",
     price: 330,
@@ -48,6 +54,7 @@ const products = [
       "https://cdn.shopify.com/s/files/1/0028/1971/9281/products/Tarmac-BlackFront_1200x.png?v=1623296303",
   },
   {
+    id: 7,
     name: "Incase City Market Tote with Cordura",
     description: "a tote",
     price: 100,
