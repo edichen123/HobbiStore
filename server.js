a7101a12f75f8e709ab11136ecef5579695a48d6
edichen123 <edichenjl@outlook.com>
2022-03-10 18:11:31 +0800

    remove cart checkout

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { urlencoded } = require("express");
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
+const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const cors = require("cors");
 // Routes
@@ -15,14 +16,22 @@ const OrderRoute = require("./routes/OrderRoute");
 const StripeRoute = require("./routes/StripeRoute");
 
 //MongoDB
-const connectDB = require("./MongoDB/mongooseDB");
-connectDB();
+// const connectDB = require("./MongoDB/mongooseDB");
+// connectDB();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 //config
 const app = express();
 const PORT = process.env.PORT;
 
 // middleware
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.json());
 app.use(cors());
 app.use(methodOverride("_method"));
@@ -39,6 +48,9 @@ app.use("/api/orders", OrderRoute);
 app.use("/api/checkout", StripeRoute);
 
 //routes
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 //listen
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
